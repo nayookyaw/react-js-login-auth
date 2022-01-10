634d6aba196fe8a821425b9309c370e99f547475
thazinphyo-datumstruct <thazinphyo@datumstruct.com>
2022-01-10 20:49:03 +0800

    Login and Home screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './views/Login/Login';
+import Home from './views/Home/Home';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="wrapper">
+        <BrowserRouter>
+          <div className="container">
+            <Routes>
+              <Route path="/login" element={ <Login />} />
+              <Route path="/" element={ <Home />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
